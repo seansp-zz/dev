c0cb72d4224e3569ac6f8f851b6fe97ec86b244f
Sean Spratt <seansp@gmail.com>
2019-01-14 14:15:19 -0800

    ...

--- lsvm.rhel/test.lsvmprep.sh
+++ lsvm.rhel/test.lsvmprep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo yum install -y lsvmtools
+cp /opt/lsvmtools-1.0.0/lsvmprep /opt/lsvmtools-1.0.0/lsvmprep.backup
 cp ~/dev/lsvmprep /opt/lsvmtools-1.0.0/lsvmprep
-cd /opt/lsvmtools-1.0.0
+pushd /opt/lsvmtools-1.0.0
