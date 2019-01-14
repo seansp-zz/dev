d488189c03042bec704de86901aa7c2a18b9a66c
Sean Spratt <seansp@gmail.com>
2019-01-14 13:25:37 -0800

    listing grub entries and removing a confirm.

--- lsvm.rhel/setup.official.sh
+++ lsvm.rhel/setup.official.sh
@@ -1,4 +1,5 @@
-sudo yum install hyperv-daemons openssh-server
+#!/bin/bash
+sudo yum install -y hyperv-daemons openssh-server 
 sudo service sshd start
 
 sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm
